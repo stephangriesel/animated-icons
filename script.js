aa770f7b157ef13038923e14322255d619817278
Stephan Griesel <sgriesel@gmail.com>
2018-06-03 10:03:29 +0200

    add more icons

--- script.js
+++ script.js
@@ -10,3 +10,41 @@ function breakChain (){
 breakChain();
 
 setInterval(breakChain, 2000);
+
+function chargeBattery () {
+    let battery = document.getElementById('battery');
+    battery.innerHTML = "&#xf244;";
+    setTimeout(function (){
+        battery.innerHTML = "&#xf243;";
+    }, 1000);
+    setTimeout(function (){
+        battery.innerHTML = "&#xf242;";
+    }, 2000);
+    setTimeout(function (){
+        battery.innerHTML = "&#xf241;";
+    }, 3000);
+    setTimeout(function (){
+        battery.innerHTML = "&#xf240;";
+    }, 4000);
+}
+
+chargeBattery();
+
+setInterval(chargeBattery, 5000);
+
+function hourglassFill(){
+    let hourglass = document.getElementById('hg');
+    hourglass.innerHTML = "&#xf251;";
+    setTimeout(function (){
+        hg.innerHTML = "&#xf252;";
+    }, 1000);
+    setTimeout(function (){
+        hg.innerHTML = "&#xf253;";
+    }, 2000);
+
+}
+
+hourglassFill();
+
+setInterval(hourglassFill,3000);
+
